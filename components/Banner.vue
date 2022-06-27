69ea8d8af534f687411c256e70675784d39ab684
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-06-27 16:09:01 +0200

    add content files

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,14 +1,18 @@
 <template>
     <NuxtLink
-        to="/"
+        :to="banner.link"
         target="_blank"
         class="bg-gradient py-2 text-left text-sm text-gray-50 transition-all duration-300 hover:opacity-80 sm:text-base"
         tabindex="0"
     >
         <div class="custom-container space-x-2 px-4">
-            <span class="font-semibold">Nouveau !</span>
-            <span>Nous avons un nouveau site web.</span>
+            <span class="font-semibold">{{ banner.prefix }}</span>
+            <span>{{ banner.content }}</span>
             <span class="font-semibold">En savoir plus</span>
         </div>
     </NuxtLink>
 </template>
+
+<script setup lang="ts">
+import { banner } from "~/assets/content/global.json";
+</script>
--- components/Contact.vue
+++ components/Contact.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-2">
         <div
-            class="relative hidden bg-[url('https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80')] bg-cover bg-center md:block"
+            class="relative hidden bg-cover bg-center md:block"
+            :style="{ backgroundImage: `url('${contact.image}')` }"
         >
             <div class="bg-gradient absolute inset-0 z-0 opacity-60"></div>
         </div>
@@ -9,25 +10,27 @@
             <div class="max-w-xl">
                 <div class="space-y-2">
                     <div class="bg-gradient text-gradient font-medium">
-                        Une question ? Quelque chose ?
+                        {{ contact.subtitle }}
                     </div>
                     <div
                         class="text-4xl font-bold uppercase text-white sm:text-5xl"
                     >
-                        Nous sommes à votre disposition
+                        {{ contact.title }}
                     </div>
                     <div class="text-gray-400">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Nullam eget lacus ullamcorper, euismod felis nec,
-                        aliquet dolor. Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit. Nullam eget lacus ullamcorper, euismod
-                        felis nec, aliquet dolor.
+                        {{ contact.description }}
                     </div>
                 </div>
                 <NuxtLink to="/contact" class="mt-6 inline-block"
-                    ><Button>NOUS CONTACTER</Button></NuxtLink
+                    ><Button class="uppercase">{{
+                        contact.contact
+                    }}</Button></NuxtLink
                 >
             </div>
         </div>
     </div>
 </template>
+
+<script setup lang="ts">
+import { contact } from "~/assets/content/global.json";
+</script>
