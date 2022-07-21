930539d83c1d2f78cf093274a460dc08933adf85
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-07-21 14:45:15 +0200

    finish error page

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -10,23 +10,44 @@ interface Props {
 
 const Error: NextPage<Props> = ({ statusCode }) => {
     const router = useRouter()
-    const error = statusCode
-        ? `Erreur ${statusCode}`
-        : "Une erreurs s'est produite"
-    const { Meta } = useMeta({ title: error })
+    const { Meta } = useMeta({
+        title: statusCode
+            ? `Erreur ${statusCode}`
+            : "Une erreur s'est produite",
+    })
     return (
-        <div className="custom-container py-24 text-center">
+        <>
             <Meta />
-            <h1 className="text-5xl font-bold">{error}</h1>
-            <div className="mt-8 flex w-full justify-center space-x-2">
-                <Button onClick={() => router.back()}>Page précédente</Button>
-                <Link href="/" passHref>
-                    <Button component="a" type="gray">
-                        Accueil
-                    </Button>
-                </Link>
+            <div className="custom-container flex flex-grow flex-col px-4">
+                <div className="my-auto flex-shrink-0 py-16 sm:py-32">
+                    <p className="bg-gradient text-gradient text-base font-semibold uppercase tracking-wide">
+                        Erreur {statusCode}
+                    </p>
+                    <h1 className="mt-2 text-5xl font-bold uppercase text-gray-900">
+                        {statusCode === 404
+                            ? 'Page introuvable'
+                            : 'Problème serveur'}
+                    </h1>
+                    <p className="mt-2 text-lg text-gray-500">
+                        {statusCode === 404
+                            ? "Désolé, nous n'avons pas trouvé la page que vous recherchez."
+                            : "Une erreur s'est produite"}
+                    </p>
+
+                    <div className="mt-6 flex space-x-4">
+                        <Button onClick={() => router.back()}>
+                            Page précédente
+                        </Button>
+
+                        <Link href="/" passHref>
+                            <Button component="a" type="gray">
+                                Accueil
+                            </Button>
+                        </Link>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
