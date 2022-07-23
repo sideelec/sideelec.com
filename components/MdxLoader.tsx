9bc28ce6e2f6e2ecbb16b9c271a23bd01bdb87d3
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-07-23 16:27:30 +0200

    customize MDX link

--- components/MdxLoader.tsx
+++ components/MdxLoader.tsx
@@ -1,4 +1,26 @@
+import { MDXProvider } from '@mdx-js/react'
 import dynamic from 'next/dynamic'
+import Link from 'next/link'
+
+const CustomLink = (props: any) => {
+    const href = props.href
+    const isInternalLink =
+        href && (href.startsWith('/') || href.startsWith('#'))
+
+    if (isInternalLink) {
+        return (
+            <Link href={href}>
+                <a {...props}>{props.children}</a>
+            </Link>
+        )
+    }
+
+    return <a target="_blank" rel="noopener noreferrer" {...props} />
+}
+
+const components = {
+    a: CustomLink,
+}
 
 interface Props {
     path: string
@@ -9,9 +31,11 @@ const MdxLoader: React.FC<Props> = ({ path, props }) => {
     try {
         const Component = dynamic(() => import(`~/content/mdx${path}.mdx`))
         return (
-            <div className="prose max-w-none md:prose-xl">
-                <Component {...props} />
-            </div>
+            <MDXProvider components={components}>
+                <div className="prose max-w-none md:prose-xl">
+                    <Component {...props} />
+                </div>
+            </MDXProvider>
         )
     } catch (e) {
         return null
