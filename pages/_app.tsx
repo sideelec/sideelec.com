465bf2e0043f75678ada2d9b56d52095fd16b187
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-11-02 12:01:01 +0100

    fonts: use fontsource again

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import 'focus-visible'
 import '~/styles/main.scss'
+import '~/lib/fonts'
 import type { AppProps } from 'next/app'
 import PlausibleProvider from 'next-plausible'
 import { publicEnv as env } from '~/lib/env'
@@ -8,13 +9,6 @@ import Footer from '~/components/Layout/Footer'
 import { MetaContextValue, MetaContext } from '~/context/meta'
 import React from 'react'
 import { Toaster } from 'react-hot-toast'
-import { Barlow } from '@next/font/google'
-
-const barlow = Barlow({
-    subsets: ['latin'],
-    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-    preload: true,
-})
 
 const meta: MetaContextValue = {
     description: 'description',
@@ -25,19 +19,17 @@ const meta: MetaContextValue = {
 
 const App = ({ Component, pageProps }: AppProps) => {
     return (
-        <div className={barlow.className}>
-            <MetaContext.Provider value={meta}>
-                <PlausibleProvider domain={env.domain}>
-                    <Toaster position="bottom-right" reverseOrder={true} />
-                    {/* <Banner /> */}
-                    <Header />
-                    <main>
-                        <Component {...pageProps} />
-                    </main>
-                    <Footer />
-                </PlausibleProvider>
-            </MetaContext.Provider>
-        </div>
+        <MetaContext.Provider value={meta}>
+            <PlausibleProvider domain={env.domain}>
+                <Toaster position="bottom-right" reverseOrder={true} />
+                {/* <Banner /> */}
+                <Header />
+                <main>
+                    <Component {...pageProps} />
+                </main>
+                <Footer />
+            </PlausibleProvider>
+        </MetaContext.Provider>
     )
 }
 
