2c99a5232c5eea3ce57fb4c478868abb07897a60
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-11-02 11:54:08 +0100

    preload fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,7 @@ import { Barlow } from '@next/font/google'
 const barlow = Barlow({
     subsets: ['latin'],
     weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+    preload: true,
 })
 
 const meta: MetaContextValue = {
